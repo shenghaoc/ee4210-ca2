c9349d5a94fc03f483a565214a98934f54ddad92
shenghaoc <shenghaoc@outlook.com>
2022-03-16 20:48:19 +0800

    Add ability to send index.html

--- server.py
+++ server.py
@@ -9,5 +9,6 @@ while True:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024).decode()
     capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.send('HTTP/1.0 200 OK\r\n\r\n'.encode())
+    connectionSocket.sendfile(open("index.html", "rb"))
     connectionSocket.close()
