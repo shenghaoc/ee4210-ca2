f859d41f17709843622c75ab6c61a7f4285e76ec
shenghaoc <shenghaoc@outlook.com>
2022-03-16 21:47:11 +0800

    Add sequential requests for HTML and txt files

--- client.py
+++ client.py
@@ -2,10 +2,19 @@ from socket import *
 
 serverName = 'localhost'
 serverPort = 12000
+
+# First client socket to request HTML file
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From Server:', modifiedSentence.decode())
+clientSocket.send('GET /index.html HTTP/1.0\r\n'.encode())
+response = clientSocket.recv(1024).decode()
 clientSocket.close()
+print(response)
+
+# Second client socket to request txt file
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+clientSocket.send('GET /bio.txt HTTP/1.0\r\n'.encode())
+response = clientSocket.recv(1024).decode()
+clientSocket.close()
+print(response)
